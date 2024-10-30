9b435503209ce9c97f3721a98b8ba0e4f6c55984
jenanF <108689302+jenanF@users.noreply.github.com>
2024-10-31 00:34:54 +0300

    working

---
+++ arrays2.js
@@ -0,0 +1,69 @@
+// TASK 1
+function isArrayLengthOdd(numbers) {
+    if (numbers.length % 2 == 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// TASK 2
+function isArrayLengthEven(numbers) {
+    if (numbers.length % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// TASK 3
+function addLailaToArray(instructors) {
+
+    instructors.push('Laila');
+    //instructors_with_Laila = [instructors];
+
+    return instructors
+
+}
+
+let names = ['jenan', 'rayan', 'fahad'];
+console.log(addLailaToArray(names));
+
+// TASK 4
+function eliminateTeam(teams) {
+    eliminated = teams.pop();
+    return eliminated;
+}
+
+let teams = ['red', 'blue', 'yellow', 'green'];
+console.log(eliminateTeam(teams));
+
+// CHALLENGE 1
+function secondHalfOfArrayIfItIsEven(fruits) {
+    if (fruits.length % 2 == 0) {
+        index = fruits.length / 2;
+        return fruits.slice(index, fruits.length);
+    } else {
+        return [];
+    }
+}
+
+let fruits = ['apple', 'grape', 'kiwi', 'banana'];
+console.log(secondHalfOfArrayIfItIsEven(fruits));
+
+
+// CHALLENGE 2
+function youGottaCalmDown(shout) {
+    if (shout.includes('!')) {
+        index = shout.indexOf('!');
+        last = shout.lastIndexOf('!');
+        final = shout.slice(index, last + 1);
+        return shout - final;
+    } else {
+        return shout
+    }
+}
+
+let word = 'hello!!!!!!';
+//console.log(word.split());
+console.log(youGottaCalmDown(word));
